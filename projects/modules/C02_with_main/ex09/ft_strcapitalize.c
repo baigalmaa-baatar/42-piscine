e62d12042fb43a5b1ebcc8e63e73d80dee25ce24
bbaatar <baigalma@gmail.com>
2021-08-11 14:43:21 +0200

    copied ft_strcapitalize.c in testing with main

--- projects/modules/C02_with_main/ex09/ft_strcapitalize.c
+++ projects/modules/C02_with_main/ex09/ft_strcapitalize.c
@@ -1,17 +1,50 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/09 13:38:48 by bbaatar           #+#    #+#             */
+/*   Updated: 2021/08/11 14:40:03 by bbaatar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strcapitalize(char *str)
+char	*lowercase(char *str)
 {
-  int i;
-  i = 0;
+	int	i;
 
-  while(str[i] != '\0')
-  {
-    if((str[i]>='A' && str[i]<='Z'))
-    {
-      str[i] = str[i] + 32;
-    }
-    i++;
-  }
-  return(str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	str = lowercase(str);
+	while (str[i])
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z')))
+		{
+			while (!((str[i] >= 'a' && str[i] <= 'z')
+					|| (str[i] >= '0' && str[i] <= '9')) && str[i])
+				i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	return (str);
 }
--- projects/modules/C02_with_main/ex09/main.c
+++ projects/modules/C02_with_main/ex09/main.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 14:42:35 by bbaatar           #+#    #+#             */
+/*   Updated: 2021/08/11 14:42:42 by bbaatar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-char *ft_strcapitalize(char *str);
+char	*ft_strcapitalize(char *str);
 
-int main(void)
+int	main(void)
 {
-	char str[]	= "LOLOLaaa";
+	char	str1[] = "LOLOLaaa";
+	char	str2[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 
-	printf(" %s", ft_strcapitalize(str));
-
+	printf ("%s\n", ft_strcapitalize(str1));
+	printf ("%s\n", ft_strcapitalize(str2));
 	return (0);
 }
