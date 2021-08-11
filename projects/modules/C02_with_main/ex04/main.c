356fb2189d8559e352b3b9eff30113fa67cc30eb
bbaatar <baigalma@gmail.com>
2021-08-11 14:00:34 +0200

    changed the test

--- projects/modules/C02_with_main/ex04/main.c
+++ projects/modules/C02_with_main/ex04/main.c
@@ -4,9 +4,11 @@ int ft_str_is_lowercase(char *str);
 
 int main(void)
 {
-	char str[]	= "EEE";
+	char str1[]	= "eee";
+	char str2[] = "This is TEST";
 
-	printf(" %d ", ft_str_is_lowercase(str));
+	printf("The result is : %d\n", ft_str_is_lowercase(str1));
+	printf("The result is : %d\n", ft_str_is_lowercase(str2));
 
 	return (0);
 }
